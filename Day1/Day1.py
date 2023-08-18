f07c19186cc43f56de19f5dac9e8aa6d6a38993c
ghulamustafa-759 <133605469+ghulamustafa-759@users.noreply.github.com>
2023-08-18 06:35:26 -0700

    Add files via upload

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,1 +1,7 @@
+print("Welcome To Band Name Generator")
+firstName = input("What city did you grow up in ? \n")
+lastName = input("What was the name of your first pet ? \n")
 
+BandName = firstName + " " + lastName
+
+print(f"Your band name could be {BandName} .")
---
+++ Day1/README.txt
@@ -0,0 +1,1 @@
+100 days of Python with 100 projects beginner to advanced, Day 1, Band Name generator.
